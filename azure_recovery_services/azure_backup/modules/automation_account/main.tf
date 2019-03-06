0768e3b66d7cc53dd565db14fee4025414471531
Ryan Froggatt <rfroggatt@ANSGRP-002784.local>
2019-03-06 10:08:26 +0000

    Removing Null Resources

--- azure_recovery_services/azure_backup/modules/automation_account/main.tf
+++ azure_recovery_services/azure_backup/modules/automation_account/main.tf
@@ -34,11 +34,3 @@ resource "azurerm_automation_schedule" "backup_runbook_schedule" {
   start_time              = "${var.rubook_start_date}T22:00:00+00:00"
   description             = "Daily 10PM Schedule."
 }
-
-
-resource "null_resource" "runas_account" {
-  provisioner "local-exec" {
-    command = "create_runas_account.ps1 -SubscriptionId ${var.subscription_id} -ResourceGroup ${var.resource_group_name} -AutomationAccountName ${azurerm_automation_account.automation_account.name} -SelfSignedCertPlainPassword ${var.runas_cert_password}"
-    interpreter = ["pwsh", "-File"]
-  }
-}
