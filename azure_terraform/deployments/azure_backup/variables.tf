722143c8397fc157fa1b6b928df846dd15e10579
Ryan Froggatt <rfroggatt@ANSGRP-002784.local>
2019-03-25 13:48:35 +0000

    Adding Working Azure Backup Deployment

--- azure_terraform/deployments/azure_backup/variables.tf
+++ azure_terraform/deployments/azure_backup/variables.tf
@@ -1,73 +1,89 @@
 variable "automation_resource_group_name" {
+  type        = "string"
   default     = "RG-WE-AUTOMATION-CLOUDPEA"
   description = "Resource Group Name for the Automation Resources."
 }
 
 variable "backup_resource_group_name" {
+  type        = "string"
   default     = "RG-WE-BACKUP-CLOUDPEA"
   description = "Resource Group Name for the Backup Resources."
 }
 
 variable "location" {
+  type        = "string"
   default     = "West Europe"
   description = "Location to Create the Recovery Services Vaults."
 }
 
 variable "automation_account_name" {
+  type        = "string"
   default     = "AA-WE-CLOUDPEA-1"
   description = "The Name for the Automation Account."
 }
 
 variable "rubook_name" {
+  type        = "string"
   description = "The Name for the Automation Account Runbook."
 }
 
 variable "rubook_uri" {
+  type        = "string"
   description = "The full URI to the PowerShell Script Location."
 }
 
 variable "schedule_name" {
+  type        = "string"
   description = "The Name for the Automation Account Schedule."
 }
 
 variable "schedule_frequency" {
+  type        = "string"
   description = "The Frequency the Schedule runs on - OneTime, Day, Hour, Week, or Month"
 }
 
 variable "schedule_start_date" {
+  type        = "string"
   description = "The Date for the Runbook to Start in the format - 2019-03-31"
 }
 
 variable "schedule_start_date" {
+  type        = "string"
   description = "The Time for the Runbook to Start in the format - 22:00:00"
 }
 
 variable "vault_name" {
+  type        = "string"
   default     = "RSV-WE-CLOUDPEA-1"
   description = "The Name Prefix of the Recovery Services Vaults."
 }
 
 variable "tier1_vm_count" {
-  default     = 50
+  type        = "string"
+  default     = "10"
   description = "The number of Tier 1 Virtual Machines to Backup."
 }
 
 variable "tier2_vm_count" {
-  default     = 50
+  type        = "string"
+  default     = "10"
   description = "The number of Tier 2 Virtual Machines to Backup."
 }
 
 variable "tier3_vm_count" {
-  default     = 50
+  type        = "string"
+  default     = "10"
   description = "The number of Tier 3 Virtual Machines to Backup."
 }
 
 variable "tier4_vm_count" {
-  default     = 50
+  type        = "string"
+  default     = "10"
   description = "The number of Tier 4 Virtual Machines to Backup."
 }
 
 variable "tag_owner" {
-  default     = "Ryan Froggatt"
+  type        = "string"
+  default     = "Backup Team"
   description = "An Owner Tag for the Azure Backup Vaults."
 }
--- azure_terraform/modules/azure_backup/main.tf
+++ azure_terraform/modules/azure_backup/main.tf
@@ -1,27 +1,38 @@
+# Create GRS Recovery Services Vault
 resource "azurerm_recovery_services_vault" "vault_grs" {
   name                = "${var.vault_name}-GRS"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   sku                 = "Standard"
 
+  provisioner "local-exec" {
+    command     = "az backup vault backup-properties set --name ${var.vault_name}-GRS --resource-group ${var.resource_group_name} --backup-storage-redundancy GeoRedundant"
+  }
+
   tags {
     Application = "Azure Backup"
     Owner       = "${var.tag_owner}"
   }
 }
 
+# Create LRS Recovery Services Vault
 resource "azurerm_recovery_services_vault" "vault_lrs" {
   name                = "${var.vault_name}-LRS"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   sku                 = "Standard"
 
+  provisioner "local-exec" {
+    command     = "az backup vault backup-properties set --name ${var.vault_name}-LRS --resource-group ${var.resource_group_name} --backup-storage-redundancy LocallyRedundant"
+  }
+
   tags {
     Application = "Azure Backup"
     Owner       = "${var.tag_owner}"
   }
 }
 
+# Create Tier 1 Protection Policies
 resource "azurerm_recovery_services_protection_policy_vm" "policy_tier1" {
   name                = "BP-TIER1-GRS-${count.index}"
   count               = "${var.tier1_policy_count}"
@@ -51,6 +62,7 @@ resource "azurerm_recovery_services_protection_policy_vm" "policy_tier1" {
   }
 }
 
+# Create Tier 2 Protection Policies
 resource "azurerm_recovery_services_protection_policy_vm" "policy_tier2" {
   name                = "BP-TIER2-GRS-${count.index}"
   count               = "${var.tier2_policy_count}"
@@ -74,6 +86,7 @@ resource "azurerm_recovery_services_protection_policy_vm" "policy_tier2" {
   }
 }
 
+# Create Tier 3 Protection Policies
 resource "azurerm_recovery_services_protection_policy_vm" "policy_tier3" {
   name                = "BP-TIER3-LRS-${count.index}"
   count               = "${var.tier3_policy_count}"
@@ -97,6 +110,7 @@ resource "azurerm_recovery_services_protection_policy_vm" "policy_tier3" {
   }
 }
 
+# Create Tier 4 Protection Policies
 resource "azurerm_recovery_services_protection_policy_vm" "policy_tier4" {
   name                = "BP-TIER4-LRS-${count.index}"
   count               = "${var.tier4_policy_count}"
@@ -113,20 +127,4 @@ resource "azurerm_recovery_services_protection_policy_vm" "policy_tier4" {
   retention_daily = {
     count = 7
   }
-}
-
-resource "null_resource" "vault_storage_redundancy" {
-  provisioner "local-exec" {
-    command     = "az backup vault backup-properties set --name ${var.vault_name}-GRS --resource-group ${var.resource_group_name} --backup-storage-redundancy GeoRedundant"
-    interpreter = ["/bin/bash", "-c"]
-  }
-
-  provisioner "local-exec" {
-    command     = "az backup vault backup-properties set --name ${var.vault_name}-LRS --resource-group ${var.resource_group_name} --backup-storage-redundancy LocallyRedundant"
-    interpreter = ["/bin/bash", "-c"]
-  }
-  depends_on = [
-    "azurerm_recovery_services_vault.vault_grs",
-    "azurerm_recovery_services_vault.vault_lrs"
-  ] 
 }
