191d5235790e6109ce5bef82ffe97fedfa32add5
Ryan Froggatt <rfroggatt@ANSGRP-002784.local>
2019-03-25 16:04:35 +0000

    Updating Variable Descriptions

--- azure_terraform/deployments/kubernetes/variables.tf
+++ azure_terraform/deployments/kubernetes/variables.tf
@@ -1,15 +1,89 @@
-variable "resourcegroup_name" {default = "RG-PRD-UKS-CLOUDPEA-AKS"}
-variable "location" {default = "UK South"}
-variable "tag_application" {default = "CloudPea App"}
-variable "tag_environment" {default = "Production"}
-variable "tag_criticality" {default = "Tier 1"}
-variable "tag_owner" {default = "Ryan Froggatt"}
-variable "ad_app_name" {default = "ad_app_prd_uks_aks_cloudpea"}
-variable "aks_cluster_name" {default = "AKS-PRD-UKS-CLOUDPEA"}
-variable "aks_dns_prefix" {default = "cloudpea"}
-variable "node_count" {default = "1"}
-variable "node_type" {default = "Standard_B2s"}
-variable "node_disksize" {default = "30"}
-variable "acr_name" {default = "acrprdukscloudpea"}
-variable "keyvault_name" {default = "KV-PRD-UKS-CLOUDPEA"}
-variable "storage_account_name" {default = "saprdukscloudpea"}
+variable "resourcegroup_name" {
+  type        = "string"
+  default     = "RG-PRD-UKS-CLOUDPEA-AKS"
+  description = "Resource Group Name for the Azure Kubernetes Resources."
+}
+
+variable "location" {
+  type        = "string"
+  default     = "UK South"
+  description = "Location to Create the Azure Kubernetes Resources."
+}
+
+variable "tag_application" {
+  type        = "string"
+  default     = "CloudPea App"
+  description = "An Application Tag for the Azure Kubernetes Resources."
+}
+
+variable "tag_environment" {
+  type        = "string"
+  default     = "Production"
+  description = "An Environment Tag for the Azure Kubernetes Resources."
+}
+
+variable "tag_criticality" {
+  type        = "string"
+  default     = "Tier 1"
+  description = "A Criticality Tag for the Azure Kubernetes Resources."
+}
+
+variable "tag_owner" {
+  type        = "string"
+  default     = "Ryan Froggatt"
+  description = "An Owner Tag for the Azure Kubernetes Resources."
+}
+
+variable "ad_app_name" {
+  type        = "string"
+  default     = "ad_app_prd_uks_aks_cloudpea"
+  description = "Name of the Azure AD Service Principal for the Kubernetes Cluster."
+}
+
+variable "aks_cluster_name" {
+  type        = "string"
+  default     = "AKS-PRD-UKS-CLOUDPEA"
+  description = "Name of the Azure Kubernetes Cluster."
+}
+
+variable "aks_dns_prefix" {
+  type        = "string"
+  default     = "cloudpea"
+  description = "DNS Prefix for the Azure Kubernetes Cluster."
+}
+
+variable "node_count" {
+  type        = "string"
+  default     = "1"
+  description = "Number of Azure Kubernetes Worker Nodes."
+}
+
+variable "node_type" {
+  type        = "string"
+  default     = "Standard_B2s"
+  description = "Size of the Azure Kubernetes Worker Nodes."
+}
+
+variable "node_disksize" {
+  type        = "string"
+  default     = "30"
+  description = "OS Disk size of Azure Kubernetes Worker Nodes."
+}
+
+variable "acr_name" {
+  type        = "string"
+  default     = "acrprdukscloudpea"
+  description = "Azure Container Registry Name."
+}
+
+variable "keyvault_name" {
+  type        = "string"
+  default     = "KV-PRD-UKS-CLOUDPEA"
+  description = "Azure KeyVault Name."
+}
+
+variable "storage_account_name" {
+  type        = "string"
+  default     = "saprdukscloudpea"
+  description = "Azure Storage Account Name."
+}
