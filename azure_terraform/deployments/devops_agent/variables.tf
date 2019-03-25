d2db99b03496dc787fad6f463f0d362847c2a11b
Ryan Froggatt <rfroggatt@ANSGRP-002784.local>
2019-03-25 15:53:05 +0000

    Adding Variable Descriptions

--- azure_terraform/deployments/devops_agent/variables.tf
+++ azure_terraform/deployments/devops_agent/variables.tf
@@ -1,41 +1,87 @@
 variable "resourcegroup_name" {
-    default = "RG-PRD-UKS-DEVOPS"
+  type    = "string"
+  default = "RG-PRD-UKS-DEVOPS"
+  description = "Resource Group Name for the Azure Resources."
 }
+
 variable "location" {
-    default = "UK South"
+  type = "string"
+  default = "UK South"
+  description = "Location to Create the Azure Resources."
 }
+
 variable "tag_application" {
-    default = "Azure DevOps"
+  type    = "string"
+  default = "Azure DevOps"
+  description = "An Application Tag for the Azure Resources."
 }
+
 variable "tag_environment" {
-    default = "Production"
+  type    = "string"
+  default = "Production"
+  description = "An Environment Tag for the Azure Resources."
+
 }
+
 variable "tag_criticality" {
-    default = "Tier 2"
+  type    = "string"
+  default = "Tier 2"
+  description = "An Criticality Tag for the Azure Resources."
 }
+
 variable "tag_owner" {
-    default = "Ryan Froggatt"
+  type    = "string"
+  default = "DevOps Team"
+  description = "An Owner Tag for the Azure Resources."
 }
+
 variable "vm_name" {
-    default = "VM-PRD-UKS-DEVOPS-AGENT"
+  type    = "string"
+  default = "VM-PRD-UKS-DEVOPS-AGENT"
+  description = "Name for the DevOps Agent Virtual Machine."
 }
+
 variable "vm_size" {
-    default = "Standard_B1s"
+  type    = "string"
+  default = "Standard_B1s"
+  description = "VM Size for the DevOps Agent Virtual Machines."
 }
+
 variable "vm_username" {
-    default = "cloudpea-admin"
+  type    = "string"
+  default = "cloudpea-admin"
+  description = "Admin username for the DevOps Agent Virtual Machines."
 }
-variable "vm_password" {}
+
+variable "vm_password" {
+  type = "string"
+  description = "Admin password for the DevOps Agent Virtual Machines."
+}
+
 variable "corporate_ip" {
-    default = "10.0.1.10"
+  type    = "string"
+  default = "10.0.1.10"
+  description = "IP Address to allow SSH connections from."
 }
+
 variable "subnet_id" {
-    default = ""
+  type = "string"
+  description = "Full subnet ID to deploy the DevOps Agent Virtual Machines."
 }
+
 variable "devops_organisation" {
-    default = "cloupea"
+  type    = "string"
+  default = "cloupea"
+  description = "Azure DevOps Orgasnisation Name."
 }
-variable "devops_pat" {}
+
+variable "devops_pat" {
+  type = "string"
+  description = "Azure DevOps Personal Access Token."
+}
+
 variable "devops_pool_name" {
-    default = "cloudpea"
+  type    = "string"
+  default = "cloudpea"
+  description = "Azure DevOps Agent Pool Name."
 }
